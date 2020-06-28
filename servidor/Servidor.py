ab834bd5f373396746c926db394e0bbb766d0eda
usac201612150 <3064555520401@ingenieria.usac.edu.gt>
2020-06-28 07:51:28 -0600

    servidor clases

--- servidor/Servidor.py
+++ servidor/Servidor.py
@@ -23,47 +23,21 @@ qos = 2
 COMANDOS = 'comandos'
 USUARIOS = 'usuarios'
 GRUPO = 21
+
 COMANDO_FRR = b'\x02'
 COMANDO_ACK = b'\x05'
 COMANDO_OK = b'\x06'
 COMANDO_NO = b'\x07'
 
 Archivo_mensajes = 'mensajes.log' #RDSS aca vamos a guardar los mensajes que llegan de los topics
-############################################################
-#############CONFIGURAMOS ES LOGGING #######################
-############################################################
-#Configuracion inicial de logging
-logging.basicConfig(
-    level = logging.INFO, 
-    format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
-    )
-##############################################################
-################### CLASE SERVIDOR TCP #######################
-##############################################################
-class ServerTCP (object):
-    def __init__(self):
-        logging.basicConfig(
-            level = logging.INFO, 
-            format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
-        )
-        self.parametrosServer()
-    
-    def inicioServerTCP(self, ip, puerto, sock, usuariocola):
-        server_address = (ip,puerto)#RDSS LE DAMOS LOS PARAMETROS DE IP Y PUERTO
-        logging.info('Conectando a {} en el puerto {}'.format(*server_address))
-        sock.bind(server_address) #RDSS Levanta servidor con parametros especificados
-        sock.listen(usuariocola) #RDSS El argumento indica la cantidad de conexiones en cola
 
-    def parametrosServer(self):
-        ip = SERVER_IP
-        puerto = SERVER_PORT
-        usuariocola = 10 #RDSS indica la cantidad de conexiones en cola
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #RDSS LE INDICAMOS QUE VAMOS A TRABAJAR CON IPV4 Y CON TCP
-        self.inicioServerTCP(ip,puerto,sock,usuariocola)
-        
 ##############################################################
 ######################## CLASE MQTT ##########################
 ##############################################################
+logging.basicConfig(
+                    level=logging.INFO,
+                    format='[%(levelname)s](%(threadName)-10s) %(message)s'
+                    )
 
 class claseMQTT (object):
     def __init__(self):
@@ -72,8 +46,8 @@ class claseMQTT (object):
             format='[%(levelname)s](%(threadName)-10s) %(message)s'
         )
         self.logging=logging
-        self.ConfiguracionMQTT()
-        self.Archivo_mensajes = Archivo_mensajes
+        #self.ConfiguracionMQTT()
+        self.ServerTCP = ServerTCP()
 
     def ConfClienteMQTT(self, address, port, usuario, contrasena):
         self.mqttc = mqtt.Client(clean_session=True)
@@ -83,7 +57,8 @@ class claseMQTT (object):
         self.mqttc.username_pw_set(usuario,contrasena)
         self.mqttc.connect(address,port)
 
-    def suscripcionesTopic(self):      #creamos una lista para almacenar los datos de cada linea del archivo
+    def suscripcionesTopic(self):
+        datos = []      #creamos una lista para almacenar los datos de cada linea del archivo
         suscripciones = []         #creamos la lista donde almacenaremos las tuplas de los topics
         qos = 2                    #qos valor que nos servira para indicar que forma de transferencia preferimos
 
@@ -101,7 +76,7 @@ class claseMQTT (object):
             TuplaNusuarios = tuple(Nusuarios)   #convertimos la lista a tuplas 
             suscripciones.append(TuplaNusuarios) #agregamos la tupla a la lista de suscripciones
         self.mqttc.subscribe(suscripciones) #nos suscribimos a todos los usuarios que estan en la lista
-        self.mqttThread=thread.Thread(target=self.mqttc.loop_start,name = 'Recepcion Comandos MQTT', daemon = True)
+        self.mqttThread=threading.Thread(target=self.mqttc.loop_start,name = 'Recepcion Comandos MQTT', daemon = True)
         self.mqttThread.start()
 
     def ConfiguracionMQTT(self):
@@ -110,63 +85,90 @@ class claseMQTT (object):
         usuario = MQTT_USER
         contrasena = MQTT_PASS
         self.ConfClienteMQTT(direccion,puerto,usuario,contrasena)
-        self.SuscripcionesTopic()
+        self.suscripcionesTopic()
 
     def publishData(self, topicName, data): #Publicador simple
         self.mqttc.publish(topic = topic, payload = data,qos=0)
 
-    def ACK(self, qos=0, retain=False):
-        topic = COMANDOS+'/'+str(GRUPO)+'/'
+    def recepcionMensaje(self, data):
+        data=(str(data[0],data[1]))
+        datos = []
+        datos = data[1].decode().split('$')
+        if len(datos) == 3:
+            ComandoRecibido = datos[0]
+            usuariogrupo = datos[1]
+            tamañoarchivo = datos[2]
+        elif len(datos) == 2:
+            ComandoRecibido = datos[0]
+            usuariogrupo = datos[1]
+        if ComandoRecibido == '\x04':  #RDSS verificamos si es un mensaje de ALIVE
+            logging.info('enviar loggin')
+            self.ACK(usuariogrupo)     #RDSS Enviamos un ACK al client
+        elif ComandoRecibido == '\x03':  #RDSS verificamos si es un solicitud de transferencia de archivo
+            if self.confirmacion:
+                pass
+                #self.NotiparaEnviar(usuariogrupo, tamañoarchivo)
+    
+    def ACK(self, usuariogrupo, qos=0, retain=False):
+        topic = COMANDOS+'/'+str(GRUPO)+'/'+usuariogrupo
         mensaje = COMANDO_ACK+b'$'+bytes(usuariogrupo,'utf-8')
         self.mqttc.publish(topic,mensaje,qos,retain)
+    def OK(self, usuariogrupo, qos = 0, retain=False):
+        topic = COMANDOS+'/'+str(GRUPO)+'/'+usuariogrupo
+        mensaje = COMANDO_OK+b'$'+bytes(usuariogrupo,'utf-8')
+        self.mqttc.publish(topic,mensaje,qos,retain)
+    def NO(self, usuariogrupo, qos = 0, retain=False):
+        topic = COMANDOS+'/'+str(GRUPO)+'/'+usuariogrupo
+        mensaje = COMANDO_NO+b'$'+bytes(usuariogrupo,'utf-8')
+        self.mqttc.publish(topic,mensaje,qos,retain)   
+    def FRR(self, usuariogrupo, qos = 0, retain=False):
+        topic = COMANDOS+'/'+str(GRUPO)+'/'+usuariogrupo
+        mensaje = COMANDO_FRR+b'$'+bytes(usuariogrupo,'utf-8')+bytes(tamañoarchivo,'utf-8')
+        self.mqttc.publish(topic,mensaje,qos,retain)
 
-    def on_connect(client, userdata, rc): 
+    def on_connect(self, client, userdata, rc): 
         logging.info("Conectado al broker")  #Callback que se ejecuta cuando nos conectamos al broker
 
-    def on_message(client, userdata, msg): #Callback que se ejecuta cuando llega un mensaje al topic suscrito
-        logging.info("Ha llegado el mensaje al topic: " + str(msg.topic))  #Se muestra en pantalla informacion que ha llegado
-        logging.info("El contenido del mensaje es: " + str(msg.payload))   #vamos a almacenar el mensaje en un archivo.
-        MensajeRecibido = 'echo '+str(msg.payload) + ' > '+ Archivo_mensajes
-        os.system(MensajeRecibido)
+    def on_message(self, client, userdata, msg): #Callback que se ejecuta cuando llega un mensaje al topic suscrito
+        self.recepcionMensaje((msg.topic,msg.playload))
 
-    def on_publish(client, userdata, mid):   #Handler en caso se publique satisfactoriamente en el broker MQTT
+    def on_publish(self, client, userdata, mid):   #Handler en caso se publique satisfactoriamente en el broker MQTT
         publishText = "Publicacion satisfactoria"
-        logging.debug(publishText)
-
-######################################################
-## DATOS RECIBIDOS COMANDO, USUARIO, PESO ARCHIVO#####
-######################################################
-mqttcomando, mqttusuario, mqtttamano = leerMensaje()      
-
-########## CLASE COMANDOS #################
-class comandosmqtt (object):
-
-    #METODO CONSTRUCTOR
-    def __init__ (self, comando, IDusuario, tamano):
-        self.comando = comando
-        self.IDusuario = IDusuario
-        self.tamano = tamano
-
-    def Alive_Ack (self):
-        comando = b'\x05'
-        usuario = bytes(self.IDusuario,'utf-8')
-        tramaACK= comando+b'$'+usuario
-        topicComandos = '21/'+self.IDusuario
-        if self.comando == '02':
-            return publishData('comandos',topicComandos,tramaACK, qos = 0, retain = False)
-    def FRR (self):
-        comando = b'\x02'
-        usuario = bytes(self.IDusuario,'utf-8')
-        tamano = bytes(self.tamano,'utf-8')
-        tramaFRR = comando+b'$'+usuario+b'$'+tamano
-        topicComandos = '21/'+self.IDusuario
-        if self.comando == '03':
-            return publishData('comandos',topicComandos,tramaFRR, qos = 0, retain = False)
+        logging.info(publishText)   
 
 
-########## ENVIAMOS A LA CLASE comandosmqtt##########
-Accion = comandosmqtt(mqttcomando, mqttusuario, mqtttamano)
+############################################################
+#############CONFIGURAMOS ES LOGGING #######################
+############################################################
+#Configuracion inicial de logging
+logging.basicConfig(
+    level = logging.INFO, 
+    format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+    )
+##############################################################
+################### CLASE SERVIDOR TCP #######################
+##############################################################
+class ServerTCP (object):
+    def __init__(self):
+        logging.basicConfig(
+            level = logging.INFO, 
+            format = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+        )
+    
+    def inicioServerTCP(self, ip, puerto, sock, usuariocola):
+        server_address = (ip,puerto)#RDSS LE DAMOS LOS PARAMETROS DE IP Y PUERTO
+        logging.info('Conectando a {} en el puerto {}'.format(*server_address))
+        sock.bind(server_address) #RDSS Levanta servidor con parametros especificados
+        sock.listen(usuariocola) #RDSS El argumento indica la cantidad de conexiones en cola
 
+    def parametrosServer(self):
+        ip = SERVER_IP
+        puerto = SERVER_PORT
+        usuariocola = 10 #RDSS indica la cantidad de conexiones en cola
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #RDSS LE INDICAMOS QUE VAMOS A TRABAJAR CON IPV4 Y CON TCP
+        self.inicioServerTCP(ip,puerto,sock,usuariocola)
+        
+"""
 def RecepcionAudio ():
     while True:
         try:
@@ -192,37 +194,20 @@ def TransmisioAudio():
             conn.close()
     finally:
         sock.close()
+"""
+Examenproyecto980 = claseMQTT()
+Examenproyecto980.ConfiguracionMQTT()
 
-
-
-
-while True:
-    # Esperando conexion
-    print('Esperando conexion remota')
-    connection, clientAddress = sock.accept()
-    try:
-        print('Conexion establecida desde', clientAddress)
-
-        # Se envia informacion en bloques de BUFFER_SIZE bytes
-        # y se espera respuesta de vuelta
-        while True:
-            data = connection.recv(BUFFER_SIZE) #data ordena a tcp partir la informacion en cierto numero de bytes
-            print('Recibido: {!r}'.format(data)) #decodificamos de binario a str
-            if data: #Si se reciben datos (o sea, no ha finalizado la transmision del cliente)
-                print('Enviando data de vuelta al cliente')
-                connection.sendall(data) #envia todo lo que recibimos al cliente
-            else:
-                print('Transmision finalizada desde el cliente ', clientAddress)
-                break
-    
-    except KeyboardInterrupt:
-        sock.close()
-        logging.warning("Desconectando del broker...")
-
-    finally:
-        # Se baja el servidor para dejar libre el puerto para otras aplicaciones o instancias de la aplicacion
-        client.loop_stop() #Se mata el hilo que verifica los topics en el fondo
-        client.disconnect() #Se desconecta del broker
-        logging.info("Desconectado del broker. Saliendo...")
-        connection.close()
-        print('\n\nConexion finalizada con el servidor')
+#while True:
+#    try:
+#        pass
+#    except KeyboardInterrupt:
+#        sock.close()
+#        logging.warning("Desconectando del broker...")
+#    finally:
+#        # Se baja el servidor para dejar libre el puerto para otras aplicaciones o instancias de la aplicacion
+#        client.loop_stop() #Se mata el hilo que verifica los topics en el fondo
+#        client.disconnect() #Se desconecta del broker
+#        logging.info("Desconectado del broker. Saliendo...")
+#        connection.close()
+#        print('\n\nConexion finalizada con el servidor')
